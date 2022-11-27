69c6c3a5ced04db43b9a7268fe3d41549c3150c4
hou27 <ataj125@gmail.com>
2022-11-27 17:59:33 +0900

    test: 게시판 관련 view 테스트 작성 - #1
    게시판 관련 view를 담당할 경로를 정의해둔다

---
+++ src/main/java/com/hou27/basicboard/controller/ArticleController.java
@@ -0,0 +1,10 @@
+package com.hou27.basicboard.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RequestMapping("/articles")
+@Controller
+public class ArticleController {
+
+}
