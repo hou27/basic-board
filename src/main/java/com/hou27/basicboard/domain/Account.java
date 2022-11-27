a36ce02a00d9483a7836c66f7a08710e173251c1
hou27 <ataj125@gmail.com>
2022-11-27 21:22:43 +0900

    feat: 유저 계정 관련 구현 - #3
    account entity 작성 및 연관 entity 수정
    data.sql 재설정 후 test 코드 수정

---
+++ src/main/java/com/hou27/basicboard/domain/Account.java
@@ -0,0 +1,58 @@
+package com.hou27.basicboard.domain;
+
+import com.hou27.basicboard.domain.base.AuditingFields;
+import java.util.Objects;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Index;
+import javax.persistence.Table;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@ToString
+@Table(indexes = {
+    @Index(columnList = "createdAt"),
+    @Index(columnList = "email", unique = true)
+})
+@Entity
+public class Account extends AuditingFields {
+  @Setter
+  @Column(length = 100)
+  private String email;
+
+  @Setter
+  @Column(length = 100)
+  private String name;
+
+  @Setter
+  @Column(nullable = false)
+  private String password;
+
+
+  protected Account() {}
+
+  private Account(String email, String name, String password) {
+    this.password = password;
+    this.email = email;
+    this.name = name;
+  }
+
+  public static Account of(String email, String name, String password) {
+    return new Account(email, name, password);
+  }
+
+  @Override
+  public boolean equals(Object o) {
+    if (this == o) return true;
+    if (!(o instanceof Account userAccount)) return false;
+    return this.getId() != null && this.getId().equals(userAccount.getId());
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hash(this.getId());
+  }
+
+}
