01d94c892c5fb1cd0b0c86ef3c5ca670054dc66e
hou27 <ataj125@gmail.com>
2023-01-22 21:15:21 +0900

    feat: article controller의 articles method 구현 - #5
    account name column nullable false 설정
    articleresponse dto 작성
    articles method 구현
        - 테스트 통과

--- src/main/java/com/hou27/basicboard/controller/ArticleController.java
+++ src/main/java/com/hou27/basicboard/controller/ArticleController.java
@@ -1,18 +1,42 @@
 package com.hou27.basicboard.controller;
 
+import com.hou27.basicboard.domain.type.SearchType;
+import com.hou27.basicboard.dto.res.ArticleResponse;
+import com.hou27.basicboard.service.ArticleService;
 import java.util.List;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort.Direction;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
+@RequiredArgsConstructor
 @RequestMapping("/articles")
 @Controller
 public class ArticleController {
+
+  private final ArticleService articleService;
+
   @GetMapping
-  public String articles(Model map) {
-    map.addAttribute("articles", List.of());
+  public String articles(
+      @RequestParam(required = false) SearchType searchType,
+      @RequestParam(required = false) String searchValue,
+      @PageableDefault(size = 10, sort = "createdAt", direction = Direction.DESC) Pageable pageable,
+      Model map
+  ) {
+    map.addAttribute(
+        "articles",
+        articleService.searchArticles(
+            searchType,
+            searchValue,
+            pageable)
+            .map(ArticleResponse::from)
+    );
 
     return "articles/index";
   }
